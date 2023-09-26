a72b33da792245c0ad576d58c91068c1eb4c4957
Mithilesh <mithileshmarekar01@gmail.com>
2023-09-27 00:27:04 +0530

    Created/Loaded all the froducts from the products.js file and added it to amazon.html

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,29 +1,37 @@
 
+// This Array is now available in Products.js file
+// const products = [{
+//     image: 'images/products/athletic-cotton-socks-6-pairs.jpg',
+//     name: 'Black and Gray Athletic Cotton Socks - 6 Pairs',
+//     rating: {
+//         stars: 4.5,
+//         count: 87
+//     },
+//     priceRupee: 1090
+// },
 
-const products = [{
-    image: 'images/products/athletic-cotton-socks-6-pairs.jpg',
-    name: 'Black and Gray Athletic Cotton Socks - 6 Pairs',
-    rating: {
-        stars: 4.5,
-        count: 87
-    },
-    priceRupee: 1090
-},
-
-{
-    image: 'images/products/intermediate-composite-basketball.jpg',
-    name: 'Intermediate Size Basketball',
-    rating: { stars: 4, count: 127 },
-    priceRupee: 2095
-},
-{
-    image: 'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
-    name: 'Adults Plain Cotton T-Shirt - 2 Pack',
-    rating: {
-        stars: 4.5, count: 56
-    },
-    priceRupee: 799
-}];
+// {
+//     image: 'images/products/intermediate-composite-basketball.jpg',
+//     name: 'Intermediate Size Basketball',
+//     rating: { stars: 4, count: 127 },
+//     priceRupee: 2095
+// },
+// {
+//     image: 'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
+//     name: 'Adults Plain Cotton T-Shirt - 2 Pack',
+//     rating: {
+//         stars: 4.5, count: 56
+//     },
+//     priceRupee: 799
+// },
+// {
+//     image:'images/products/black-2-slot-toaster.jpg',
+//     name:'2 Slot Toaster Black',
+//     rating:{
+//         stars:5 ,count :  2197
+//     },
+//     priceRupee: 1899
+// }];
 
 
 // Combining Generated HTML Together
